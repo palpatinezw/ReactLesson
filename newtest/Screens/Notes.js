2f7dd40d21655e0fae40caa1c8d60ea8d1695a64
palpatinezw <emperorpalpatinezw@gmail.com>
2021-10-13 09:49:26 +0800

    DB

--- newtest/Screens/Notes.js
+++ newtest/Screens/Notes.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React , { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import * as SQLite from "expo-sqlite";
 
 import { createStackNavigator } from '@react-navigation/stack'
 import { FlatList } from 'react-native-gesture-handler';
@@ -11,16 +12,45 @@ import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
+const db = SQLite.openDatabase("notes.db");
+
 function mainScreen({navigation, route}) {
-    let [notes, setNotes] = useState([{title:"HI", text:"This is a note"}])
+    let [notes, setNotes] = useState([])
+
+    function refreshNotes() {
+        db.transaction((tx)=> {
+            tx.executeSql(
+                'SELECT * FROM notes ORDER BY id DESC', null, 
+                (txObj, {rows: { _array }}) => setNotes(_array), 
+                (txObj, error) => console.log("DB error: ", error)
+            ) 
+        })
+        console.log(notes)
+    }
 
     useEffect(() => {
+      db.transaction((tx)=> {
+        tx.executeSql(
+            'CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, text TEXT)'
+        )
+      }, null, refreshNotes) // REFRESHNOTES
+    }, [])
+    
+
+    useEffect(() => {
+        // if (route.params?.title) {
+        //     setNotes([
+        //         ...notes,
+        //         {title:route.params.title, text:route.params.text}
+        //     ])
+        // }
         if (route.params?.title) {
-            setNotes([
-                ...notes,
-                {title:route.params.title, text:route.params.text}
-            ])
+          console.log(`add notes ${route.params.text}`)
+          db.transaction((tx) => {
+              tx.executeSql(`INSERT INTO notes (title, text) VALUES (?, ?)`, [route.params.title, route.params.text])
+          }, null, refreshNotes) // REFRESHNOTES
         }
+        navigation.setParams({text:null})
     }, [route.params?.title])
 
     function renderNotes({item}) {
@@ -41,7 +71,7 @@ function mainScreen({navigation, route}) {
 
 export default function Notes() {
   return (
-    <Stack.Navigator headerMode="None">
+    <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="main" component={mainScreen} />
         <Stack.Screen name="editNote" component={editNote} />
         <Stack.Screen name="details" component={details}/>
